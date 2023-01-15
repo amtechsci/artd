d361c3860d991af22575b23e945099192abcd54b
amtechsci <37726532+amtechsci@users.noreply.github.com>
2023-01-16 04:18:04 +0530

    mc

--- admin/manage_categories.php
+++ admin/manage_categories.php
@@ -40,16 +40,28 @@ if(isset($_POST['submit'])){
 	if($msg==''){
 		if(isset($_GET['id']) && $_GET['id']!=''){
 		    if($_FILES['image']['name']!=''){
+		        $check = explode($_FILES['image']['name']);
+                $check = end($check);
+                if($check == 'jpg' or $check == 'png' or $check == 'jpeg'){
 				$cat_img=rand(111111111,999999999).'_'.$_FILES['image']['name'];
 				move_uploaded_file($_FILES['image']['tmp_name'],BANNER_SERVER_PATH.$cat_img);
 				mysqli_query($con,"update categories set categories='$categories',cat_img='$cat_img' where id='$id'");
+                }else{
+				mysqli_query($con,"update categories set categories='$categories' where id='$id'");
+			    }
 			}else{
 				mysqli_query($con,"update categories set categories='$categories' where id='$id'");
 			}
 		}else{
+		    $check = explode($_FILES['image']['name']);
+            $check = end($check);
+            if($check == 'jpg' or $check == 'png' or $check == 'jpeg'){
 		    $cat_img = rand(111111111,999999999).'_'.$_FILES['image']['name'];
 			move_uploaded_file($_FILES['image']['tmp_name'],BANNER_SERVER_PATH.$cat_img);
 			mysqli_query($con,"insert into categories(categories,cat_img,status) values('$categories','$cat_img','1')");
+            }else{
+			mysqli_query($con,"insert into categories(categories,cat_img,status) values('$categories','https://artdarshan.com/art/logo.pngs','1')");
+            }
 		}
 		header('location:categories.php');
 		die();
